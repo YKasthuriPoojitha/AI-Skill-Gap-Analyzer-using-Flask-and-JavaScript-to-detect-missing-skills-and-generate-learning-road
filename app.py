2f43e9102ad7c03409654d2d6ba88504a1f243e7
Poojitha <kpoojitha1603@gmail.com>
2026-03-21 16:32:44 +0530

    final fit port issue

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 1000))
+    app.run(host='0.0.0.0', port=port)
